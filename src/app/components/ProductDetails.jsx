c5b631cc17fb7a21f3ba55ca28958c18f1077cce
Margrethe1316 <maso0012@stud.kea.dk>
2026-04-17 11:41:02 +0200

    Productdetails fe

--- src/app/components/ProductDetails.jsx
+++ src/app/components/ProductDetails.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Testimg from "../../../public/testimg.png";
 import QuantityPicker from "./QuantityPicker";
+import BtnPrimary from "./BtnPrimary";
 
-const ProductDetails = () => {
+const ProductDetails = ({ text }) => {
     return ( 
         <section className="grid grid-cols-2 gap-6 max-w-[70rem] mx-auto mt-[2rem]">
             <div className="rounded-2xl overflow-hidden">
@@ -17,6 +18,7 @@ const ProductDetails = () => {
                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
             <div>
             <QuantityPicker/>
+            <BtnPrimary text={text}/>
             </div>
             </div>
 
